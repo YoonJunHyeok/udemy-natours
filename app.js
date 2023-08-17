43b7bf1476df2542d2436884820bcb79a80ab95e
Junhyeok Yoon <jhyoon1607@gmail.com>
2023-08-17 12:41:42 +0900

    fix: for deployment

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -169,6 +170,8 @@ app.use(
     ],
   }),
 );
+
+app.use(compression());
 
 // Further HELMET configuration for Security Policy (CSP)
 // app.use(helmet());
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -10,9 +10,7 @@ export const bookTour = async (tourId) => {
 
   try {
     // 1) Get Checkout session
-    const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`,
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
 
     // 2) Redirect to checkout form
     await stripe.redirectToCheckout({
